222f27992bf4632b3a46ed2618857b02395291f4
Brian Z <brianzhouzc@gmail.com>
2021-10-12 15:10:31 -0700

    Cleanup

--- app.js
+++ app.js
@@ -5,57 +5,27 @@ var canvas;
 var gl;
 
 const disk_radius = 280.0;
+const disk_verts = [
+    // covers the entire canvas
+    // First triangle:
+    1.0, 1.0,
+    -1.0, 1.0,
+    -1.0, -1.0,
+    // Second triangle:
+    -1.0, -1.0,
+    1.0, -1.0,
+    1.0, 1.0
+];
 
 function main() {
-    canvas = document.getElementById("glCanvas");
-    gl = canvas.getContext("webgl");
+    init();
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
+    function logic() {
+        canvas = document.getElementById("glCanvas");
+        gl = canvas.getContext("webgl");
 
-    var bacteriaVertexShader = gl.createShader(gl.VERTEX_SHADER);
-    var bacteriaFragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    var diskVertexShader = gl.createShader(gl.VERTEX_SHADER);
-    var diskFragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    compileShader(gl, bacteriaVertexShader, bacteriaVertexShaderText);
-    compileShader(gl, bacteriaFragmentShader, bacteriaFragmentShaderText);
-    compileShader(gl, diskVertexShader, diskVertexShaderText);
-    compileShader(gl, diskFragmentShader, diskFragmentShaderText);
-
-    var bacteriaProgram = gl.createProgram();
-    var diskProgram = gl.createProgram();
-
-    gl.attachShader(bacteriaProgram, bacteriaVertexShader);
-    gl.attachShader(bacteriaProgram, bacteriaFragmentShader);
-    gl.attachShader(diskProgram, diskVertexShader);
-    gl.attachShader(diskProgram, diskFragmentShader);
-
-    linkProgram(gl, bacteriaProgram);
-    linkProgram(gl, diskProgram);
-
-    var diskVerts = [
-        // covers the entire canvas
-        // First triangle:
-        1.0, 1.0,
-        -1.0, 1.0,
-        -1.0, -1.0,
-        // Second triangle:
-        -1.0, -1.0,
-        1.0, -1.0,
-        1.0, 1.0
-    ];
-
-
-    //var circlevert = generateCircleVerticies(0, 0, 0, '');
-    Bacteria.allBacterias.push(Bacteria.generateRandomBacteria());
-    Bacteria.allBacterias.push(Bacteria.generateRandomBacteria());
-    console.log(Bacteria.allBacterias)
-
-    var circlevert = [];
-
-
-    var circlebufferobj = gl.createBuffer();
-    var diskVertexBufferObj = gl.createBuffer();
+        gl.viewport(0, 0, canvas.width, canvas.height);
+    }
 
     function render() {
         gl.clearColor(0.968627451, 0.929411765, 0.88627451, 1);
@@ -63,72 +33,21 @@ function main() {
         gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
         gl.enable(gl.BLEND);
 
-        circlevert = [];
-        console.log(Bacteria.allBacterias.length)
 
+        /*
         if (Bacteria.allBacterias.length > 1) {
             for (var i = 0; i < Bacteria.allBacterias.length - 1; i++) {
                 if (Bacteria.allBacterias[i].colideWith(Bacteria.allBacterias[i + 1]))
                     Bacteria.allBacterias.splice(i + 1, 1)
             }
         }
-
+*/
         Bacteria.allBacterias.forEach(bacteria => {
-            Bacteria.allBacterias[i].radius += 1;
-            circlevert.push(...bacteria.generateVerticies());
+            bacteria.radius += 0.1;
         })
 
-        gl.useProgram(bacteriaProgram);
-        gl.bindBuffer(gl.ARRAY_BUFFER, circlebufferobj);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(circlevert), gl.STATIC_DRAW);
-        var cpal = gl.getAttribLocation(bacteriaProgram, 'v_position');
-        gl.vertexAttribPointer(
-            cpal, //attribute location
-            2, //number of elements per attribute
-            gl.FLOAT,
-            gl.FALSE,
-            5 * Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
-            0 * Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
-        );
-        var ccolor = gl.getAttribLocation(bacteriaProgram, 'v_color');
-        gl.vertexAttribPointer(
-            ccolor, //attribute location
-            3, //number of elements per attribute
-            gl.FLOAT,
-            gl.FALSE,
-            5 * Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
-            2 * Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
-        );
-
-        gl.enableVertexAttribArray(cpal);
-        gl.enableVertexAttribArray(ccolor);
-
-        gl.drawArrays(gl.TRIANGLES, 0, circlevert.length / 5 * 2);
-
-
-        gl.useProgram(diskProgram);
-        gl.bindBuffer(gl.ARRAY_BUFFER, diskVertexBufferObj);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(diskVerts), gl.STATIC_DRAW);
-        var positionAttribLocation = gl.getAttribLocation(diskProgram, 'v_position');
-        gl.vertexAttribPointer(
-            positionAttribLocation, //attribute location
-            2, //number of elements per attribute
-            gl.FLOAT,
-            gl.FALSE,
-            2 * Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
-            0 * Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
-        );
-        gl.enableVertexAttribArray(positionAttribLocation);
-        var widthHandle = gl.getUniformLocation(diskProgram, "center_x");
-        var heightHandle = gl.getUniformLocation(diskProgram, "center_y");
-        var radiusHandle = gl.getUniformLocation(diskProgram, "radius");
-
-        //radius += 1;
-        gl.uniform1f(widthHandle, canvas.width / 2);
-        gl.uniform1f(heightHandle, canvas.height / 2);
-        gl.uniform1f(radiusHandle, disk_radius);
-
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        draw_bacterias(circlevert);
+        draw_disk();
 
         window.requestAnimationFrame(render);
     }
@@ -163,36 +82,100 @@ function main() {
 }
 window.onload = main;
 
-// all in pixels
-function generateCircleVerticies(cx, cy, radius, color = vec3(0.0, 0.0, 0.0)) {
-    //var verts = screen2vert(cx, cy);
-    var verts = [];
-    /*
-    x, y,    r, g, b,
-    x, y,    r, g, b
-    */
-    var num_fans = 50;
-    var angle = 2 * Math.PI / num_fans;
-    var center = screen2vert(cx, cy)
+var bacteriaProgram, diskProgram;
+function init() {
+    //Initialize webgl
+    canvas = document.getElementById("glCanvas");
+    gl = canvas.getContext("webgl");
 
-    for (var i = 0; i < num_fans; i++) {
-        verts.push(...center);
-        verts.push(...color);
+    gl.viewport(0, 0, canvas.width, canvas.height);
 
-        var a = i * angle;
-        var x = Math.cos(a) * radius + cx;
-        var y = Math.sin(a) * radius + cy;
-        verts.push(...screen2vert(x, y));
-        verts.push(...color)
+    var bacteriaVertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var bacteriaFragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    var diskVertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var diskFragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-        var a = (i + 1) * angle;
-        var x = Math.cos(a) * radius + cx;
-        var y = Math.sin(a) * radius + cy;
-        verts.push(...screen2vert(x, y));
-        verts.push(...color)
-    }
-    console.log(verts)
-    return verts;
+    compileShader(gl, bacteriaVertexShader, bacteriaVertexShaderText);
+    compileShader(gl, bacteriaFragmentShader, bacteriaFragmentShaderText);
+    compileShader(gl, diskVertexShader, diskVertexShaderText);
+    compileShader(gl, diskFragmentShader, diskFragmentShaderText);
+
+    bacteriaProgram = gl.createProgram();
+    diskProgram = gl.createProgram();
+
+    gl.attachShader(bacteriaProgram, bacteriaVertexShader);
+    gl.attachShader(bacteriaProgram, bacteriaFragmentShader);
+    gl.attachShader(diskProgram, diskVertexShader);
+    gl.attachShader(diskProgram, diskFragmentShader);
+
+    linkProgram(gl, bacteriaProgram);
+    linkProgram(gl, diskProgram);
+
+    //Initialize logic
+    for (var i = 0; i < 5; i++)
+        Bacteria.allBacterias.push(Bacteria.generateRandomBacteria());
+}
+
+function draw_bacterias() {
+    gl.useProgram(bacteriaProgram);
+
+    var bacteriaVerticies = [];
+    Bacteria.allBacterias.forEach(bacteria => {
+        bacteriaVerticies.push(...bacteria.generateVerticies());
+    })
+
+    var bacteriaBufferObj = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bacteriaBufferObj);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(bacteriaVerticies), gl.STATIC_DRAW);
+    var bacteriaPosAttrLocation = gl.getAttribLocation(bacteriaProgram, 'v_position');
+    gl.vertexAttribPointer(
+        bacteriaPosAttrLocation, //attribute location
+        2, //number of elements per attribute
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
+        0 * Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
+    );
+    var bacteriaColorAttrLocation = gl.getAttribLocation(bacteriaProgram, 'v_color');
+    gl.vertexAttribPointer(
+        bacteriaColorAttrLocation, //attribute location
+        3, //number of elements per attribute
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
+        2 * Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.enableVertexAttribArray(bacteriaPosAttrLocation);
+    gl.enableVertexAttribArray(bacteriaColorAttrLocation);
+
+    gl.drawArrays(gl.TRIANGLES, 0, bacteriaVerticies.length / 5 * 2);
+}
+function draw_disk() {
+    gl.useProgram(diskProgram);
+
+    var diskBufferObj = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, diskBufferObj);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(disk_verts), gl.STATIC_DRAW);
+    var diskPosAttrLocation = gl.getAttribLocation(diskProgram, 'v_position');
+    gl.vertexAttribPointer(
+        diskPosAttrLocation, //attribute location
+        2, //number of elements per attribute
+        gl.FLOAT,
+        gl.FALSE,
+        2 * Float32Array.BYTES_PER_ELEMENT,//size of an individual vertex
+        0 * Float32Array.BYTES_PER_ELEMENT//offset from the beginning of a single vertex to this attribute
+    );
+    gl.enableVertexAttribArray(diskPosAttrLocation);
+    var widthHandle = gl.getUniformLocation(diskProgram, "center_x");
+    var heightHandle = gl.getUniformLocation(diskProgram, "center_y");
+    var radiusHandle = gl.getUniformLocation(diskProgram, "radius");
+
+    gl.uniform1f(widthHandle, canvas.width / 2);
+    gl.uniform1f(heightHandle, canvas.height / 2);
+    gl.uniform1f(radiusHandle, disk_radius);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
 function getRandomPointOnCircumference(cx, cy, radius) {
@@ -249,6 +232,7 @@ class Bacteria {
     ];
 
     constructor(center_x, center_y, radius, color = vec3(1.0, 0.0, 0.0)) {
+        this.spawn_time = Date.now();
         this.center_x = center_x;
         this.center_y = center_y;
         this.radius = radius;
